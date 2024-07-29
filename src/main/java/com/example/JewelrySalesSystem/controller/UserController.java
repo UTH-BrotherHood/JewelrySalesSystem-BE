31bea9b6b0d163a88973fe15031ed8b03354c273
kienhoang3397 <125807253+kienhoang3397@users.noreply.github.com>
2024-07-29 16:02:13 +0700

    chore: Add CRUD operations for User entity in UserController and UserService

--- src/main/java/com/example/JewelrySalesSystem/controller/UserController.java
+++ src/main/java/com/example/JewelrySalesSystem/controller/UserController.java
@@ -1,20 +1,42 @@
 package com.example.JewelrySalesSystem.controller;
 
 import com.example.JewelrySalesSystem.dto.request.UserCreationRequest;
+import com.example.JewelrySalesSystem.dto.request.UserUpdateRequest;
 import com.example.JewelrySalesSystem.entity.User;
 import com.example.JewelrySalesSystem.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("/users")
 public class UserController {
     @Autowired
     private UserService userService;
 
-    @PostMapping("/users")
+    @PostMapping
     User createUser(@RequestBody UserCreationRequest request) {
        return userService.createRequest(request) ;
+    }
+    @GetMapping
+    List<User> getUsers() {
+        return userService.getUsers();
+    }
+
+    @GetMapping("/{userId}")
+    User getUser(@PathVariable("userId") String userId){
+        return userService.getUser(userId);
+    }
+
+    @PutMapping("/{userId}")
+    User updateUser(@PathVariable String userId, @RequestBody UserUpdateRequest request){
+           return userService.updateUser(userId, request);
+
+    }
+    @DeleteMapping("/{userId}")
+    String deleteUser(@PathVariable String userId) {
+        userService.deleteUser(userId);
+        return "User has been deleted";
     }
 }
--- src/main/java/com/example/JewelrySalesSystem/service/UserService.java
+++ src/main/java/com/example/JewelrySalesSystem/service/UserService.java
@@ -1,10 +1,13 @@
 package com.example.JewelrySalesSystem.service;
 
 import com.example.JewelrySalesSystem.dto.request.UserCreationRequest;
+import com.example.JewelrySalesSystem.dto.request.UserUpdateRequest;
 import com.example.JewelrySalesSystem.entity.User;
 import com.example.JewelrySalesSystem.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class UserService {
@@ -23,5 +26,30 @@ public class UserService {
 
         return userRepository.save(user);
 
+    }
+
+    public List<User> getUsers(){
+        return userRepository.findAll();
+    }
+
+    public User getUser(String id){
+        return userRepository.findById(id)
+                .orElseThrow(()->new RuntimeException("User Not Found"));
+    }
+
+    public User updateUser(String userID, UserUpdateRequest request){
+        User user = getUser(userID);
+
+        user.setPassword(request.getPassword());
+        user.setEmail(request.getEmail());
+        user.setPhone(request.getPhone());
+        user.setAddress(request.getAddress());
+        user.setRewardPoints(request.getRewardPoints());
+
+        return userRepository.save(user);
+    }
+
+    public void deleteUser(String userId){
+        userRepository.deleteById(userId);
     }
 }
