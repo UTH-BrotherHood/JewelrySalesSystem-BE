4f7ecbc21e9b4dfcac5e0470f1f4bfbd77ed820c
thanhnunu26 <166847981+thanhnunu26@users.noreply.github.com>
2024-08-03 12:56:28 +0700

    Add files via upload

---
+++ src/main/java/com/example/JewelrySalesSystem/repository/PromotionRepository.java
@@ -0,0 +1,9 @@
+package com.example.JewelrySalesSystem.repository;
+
+import com.example.JewelrySalesSystem.entity.Promotion;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PromotionRepository extends JpaRepository<Promotion, Integer> {
+}
