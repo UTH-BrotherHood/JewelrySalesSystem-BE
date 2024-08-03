b4d6693c76e7e99784e376258b51ece3dbca0c6c
Lou1sVuong <110114506+Lou1sVuong@users.noreply.github.com>
2024-08-03 12:51:31 +0700

    Delete PromotionRepository.java

--- PromotionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.JewelrySalesSystem.repository;
-
-import com.example.JewelrySalesSystem.entity.Promotion;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PromotionRepository extends JpaRepository<Promotion, Integer> {
-}
